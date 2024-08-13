0b2ab48b4fdc6090fc21c010479c53763c07c979
Avaray <1459553+Avaray@users.noreply.github.com>
2024-08-13 20:01:35 +0200

    second attempt

--- sig.php
+++ sig.php
@@ -10,7 +10,7 @@ chdir(dirname(__FILE__));
 // Declare global variables
 $steam_ids = array();
 $steam_api_key = '';
-$config = [];
+$config = array();
 $database = array();
 
 // Custom echo function with timestamp
